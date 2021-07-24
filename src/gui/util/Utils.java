e8cf4904f5061076fd5cb79240b282935471ca2f
joaomarcuscardoso <rfcjoujou@gmail.com>
2021-07-24 20:59:22 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -1,11 +1,21 @@
 package gui.util;
 
 import javafx.event.ActionEvent;
-import javafx.stage.Stage;
 import javafx.scene.Node;
+import javafx.stage.Stage;
 
 public class Utils {
     public static Stage currentStage(ActionEvent event) {
         return (Stage) ((Node) event.getSource()).getScene().getWindow();
+    }
+
+    public static Integer tryParseToInt(String str) {
+        try {
+
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+
     }
 }
